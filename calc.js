88889bee9d069d5b71118534535321db8cc52e95
AbubakarAli23 <abubakarbafadal3@gmail.com>
2025-09-21 15:59:31 +0300

    added calculator functionality

--- calc.js
+++ calc.js
@@ -1,12 +1,59 @@
 let display = document.querySelector("#display");
-display.textContent = "hello";
-
+let arr = [];
 function input() {
+
     let buttons = document.getElementsByTagName("button");
-    for (let button of buttons) {
-        button.addEventListener("click", function () {
-            display.textContent = button.textContent
-        })
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function () {
+            let value = buttons[i].textContent;
+            if (value === "=") {
+                calculation()
+            } else if (value === "c") {
+                arr = [];
+                display.textContent = ""
+            }
+            else if (value === "delete") {
+                arr.pop(value);
+                display.textContent = arr.join("");
+            }
+            else {
+                display.textContent += value;
+                arr.push(value);
+            }
+        });
+
     }
+
 }
-input()
+
+
+function calculation() {
+    let notJoined = arr;
+    if (notJoined.includes("+") && notJoined.includes("-")) {
+        calculation();
+    }
+    else if (notJoined.includes("+")) {
+        let Joined = notJoined.join("");
+        let splitted = Joined.split("+")
+        let ans = parseFloat(splitted[0]) + parseFloat(splitted[1]);
+        display.textContent = ans;
+    } else if (notJoined.includes("/")) {
+        let Joined = notJoined.join("");
+        let splitted = Joined.split("/")
+        let ans = parseFloat(splitted[0]) / parseFloat(splitted[1]);
+        display.textContent = ans;
+    } else if (notJoined.includes("*")) {
+        let Joined = notJoined.join("");
+        let splitted = Joined.split("*")
+        let ans = parseFloat(splitted[0]) * parseFloat(splitted[1]);
+        display.textContent = ans;
+    } else if (notJoined.includes("-")) {
+        let Joined = notJoined.join("");
+        let splitted = Joined.split("-")
+        let ans = parseFloat(splitted[0]) - parseFloat(splitted[1]);
+        display.textContent = ans;
+    }
+
+}
+
+input();
